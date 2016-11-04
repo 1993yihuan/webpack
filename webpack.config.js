b937589b9f552094df8e317c21b5bfbd36709842
heyihuan <heyihuan@goluk.com>
2016-11-04 17:29:53 +0800

    add webpack-html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,52 @@ var autoprefixer = require('autoprefixer');
 var postcsscenter = require('postcss-center');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//定义入口文件
+var entryArr = {
+    main:'./js/main.js',
+    index:'./js/index.js',
+    public:['./lib/jquery.js']
+};
+//定义插件
+var pluginsArr = [
+    new webpack.BannerPlugin('build in '+new Date().toLocaleString()+' by heyihuan'),//增加头部信息
+    new ExtractTextPlugin('./css/[name].build.css'),//独立css文件
+    new webpack.ProvidePlugin({
+        $:"jquery",
+        jQuery:"jquery",
+        "window.jQuery":"jquery"
+    }),//全局jqury文件
+    // new HtmlWebpackPlugin({
+    //    title: 'Custom template',
+    //    filename: './html-plugin.html',
+    //    template: './html-plugin.html', // Load a custom template
+    //    inject: 'head', // Inject all scripts into the body
+    //    chunks: ['index']
+    // }),
+    new CleanWebpackPlugin('dist')//清空输出文件夹
+];
+
+var pageArr = [
+    'html-plugin',
+    'index',
+    'main'
+];
+pageArr.forEach((page) => {
+  var htmlPlugin = new HtmlWebpackPlugin({
+        title: 'Custom template',
+        filename: 'views/'+page+'.html',
+        template: 'views/'+page+'.html',
+        inject: 'head',
+        chunks: [page]
+    });
+  pluginsArr.push(htmlPlugin);
+});
 
 module.exports = {
     devtool: 'source-map',
-    entry: {
-        main:'./js/main.js',
-        index:'./js/index.js',
-        public:['./lib/jquery.js'],
-    },
+    entry: entryArr,
     output: {
         path: "./dist/", // 输出文件的保存路径
         filename: 'js/[name].build.js', // 输出文件的名称
@@ -29,17 +67,8 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new webpack.BannerPlugin('build in '+new Date().toLocaleString()+' by heyihuan'),//增加头部信息
-        new ExtractTextPlugin('./css/[name].build.css'),//独立css文件
-        new webpack.ProvidePlugin({
-            $:"jquery",
-            jQuery:"jquery",
-            "window.jQuery":"jquery"
-        }),//全局jqury文件
-        new CleanWebpackPlugin('dist')//清空输出文件夹
-    ],
+    plugins: pluginsArr,
     postcss: function () {
         return [precss, autoprefixer, postcsscenter];
     }
-  }
+ }
