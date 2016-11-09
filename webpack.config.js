865e9f8341f2e3799af8b5b10b75790dc1b255ae
heyihuan <heyihuan@goluk.com>
2016-11-09 17:51:59 +0800

    beatiful config

--- webpack.config.js
+++ webpack.config.js
@@ -5,41 +5,83 @@
  * //Linux or OSX
  * export NODE_ENV=test
  */
-var webpack = require('webpack');
-var path = require('path');
-var util=require('util');
+'use strict';
 
-var precss       = require('precss');
-var autoprefixer = require('autoprefixer');
-var postcsscenter = require('postcss-center');
-var postcssmixins = require('postcss-mixins');
-var postcssnested = require('postcss-nested');
-var postcssvars = require('postcss-simple-vars');
-var postcssfor = require('postcss-for');
-var postcsscalc = require('postcss-calc');
 
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+/*====================package文件====================*/
+const webpack = require('webpack');
+const path = require('path');
+const util=require('util');
+const fs = require('fs');
 
-// 遍历views文件
-var fs = require('fs');
+const precss       = require('precss');
+const autoprefixer = require('autoprefixer');
+const postcsscenter = require('postcss-center');
+const postcssmixins = require('postcss-mixins');
+const postcssnested = require('postcss-nested');
+const postcssvars = require('postcss-simple-vars');
+const postcssfor = require('postcss-for');
+const postcsscalc = require('postcss-calc');
 
-//定义环境变量地址
-var API = {
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+/*====================package文件====================*/
+
+
+
+/*====================初始化对象====================*/
+let webpackObj = {};
+/*!====================初始化对象====================*/
+
+
+
+/*====================环境变量地址====================*/
+let API = {
 	test: 'http://test.api.xx.com',
 	pro: 'http://api.xx.com',
 	dev: 'http://dev.api.xx.com'
 };
-//定义入口文件
-var entryArr = {
+/*!====================环境变量地址====================*/
+
+
+
+/*====================入口文件====================*/
+let entryArr = {
     public:['./lib/jquery.js']
 };
 fs.readdirSync('js/entry').forEach((page) => {
 	entryArr[page.replace('.js','')] = './js/entry/'+page
 })
-//定义插件
-var pluginsArr = [
+/*!====================入口文件====================*/
+
+
+
+/*====================输出路径====================*/
+let output = {
+	path: "./dist/", // 输出文件的保存路径
+	filename: 'js/[name].build.js', // 输出文件的名称
+	sourceMapFilename:'map/[file].map'
+}
+/*!====================输出路径====================*/
+
+
+
+/*====================模块====================*/
+let moduleArr = {
+	loaders: [
+		{
+			test:  /\.css$/,
+			loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader?sourceMap")
+		}
+	]
+}
+/*!====================模块====================*/
+
+
+
+/*====================插件====================*/
+let pluginsArr = [
     new webpack.BannerPlugin('build in '+new Date().toLocaleString()+' by heyihuan'),//增加头部信息
     new ExtractTextPlugin('./css/[name].build.css'),//独立css文件
     new webpack.ProvidePlugin({
@@ -64,25 +106,28 @@ fs.readdirSync('views').forEach((page) => {
     });
     pluginsArr.push(htmlPlugin);
 });
+/*!====================插件====================*/
 
-module.exports = {
+
+
+/*====================插件配置====================*/
+webpackObj.postcss = function () {
+	return [precss, autoprefixer, postcsscenter, postcssnested, postcssfor, postcsscalc, postcssvars ];
+}
+/*!====================插件配置====================*/
+
+
+
+/*====================合并变量====================*/
+webpackObj = {
     devtool: 'source-map',
     entry: entryArr,
-    output: {
-        path: "./dist/", // 输出文件的保存路径
-        filename: 'js/[name].build.js', // 输出文件的名称
-        sourceMapFilename:'map/[file].map'
-    },
-    module: {
-        loaders: [
-            {
-                test:  /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader!postcss-loader?sourceMap")
-            }
-        ]
-    },
-    plugins: pluginsArr,
-    postcss: function () {
-        return [precss, autoprefixer, postcsscenter, postcssnested, postcssfor, postcsscalc, postcssvars ];
-    }
- }
+    output: output,
+    module: moduleArr,
+    plugins: pluginsArr
+}
+/*!====================合并变量====================*/
+
+
+
+module.exports = webpackObj;
